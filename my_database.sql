f8804aac29ac3fba360b3b822647b2b40647d134
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-02-23 08:45:54 -0800

    pass Project#initialize spec

--- my_database.sql
+++ my_database.sql
@@ -15,14 +15,14 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
@@ -172,4 +172,3 @@ ALTER TABLE ONLY volunteers
 --
 -- PostgreSQL database dump complete
 --
-
