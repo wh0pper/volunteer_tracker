b807e9473447b632539cc293a2fba278439cce87
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-02-23 09:03:03 -0800

    add Project.all method and pass test

--- lib/project.rb
+++ lib/project.rb
@@ -13,6 +13,15 @@ class Project
     @id = DB.exec("SELECT id FROM projects WHERE title = '#{@title}';").first.fetch('id').to_i
   end
 
+  def self.all
+    results = DB.exec("SELECT * FROM projects;")
+    list = []
+    results.each do |result|
+      list.push(Project.new({:title => result['title'], :id => result['id'].to_i}))
+    end
+    return list
+  end
+
   def == other_project
     self.title.==(other_project.title).&(self.id.==other_project.id)
   end
