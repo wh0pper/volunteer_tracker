33deea32f8fcb39d3e95cd9f830da901c4d0ab60
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-02-23 09:43:05 -0800

    add project to Volunteer.all test to fix foreign key constraint and pass test

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -23,20 +23,22 @@ describe Volunteer do
     end
   end
 
-  # context '.all' do
-  #   it 'is empty to start' do
-  #     expect(Volunteer.all).to eq []
-  #   end
-  #
-  #   it 'returns all volunteers' do
-  #     volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-  #     volunteer1.save
-  #     volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
-  #     volunteer2.save
-  #     expect(Volunteer.all).to eq [volunteer1, volunteer2]
-  #   end
-  # end
-#
+  context '.all' do
+    it 'is empty to start' do
+      expect(Volunteer.all).to eq []
+    end
+
+    it 'returns all volunteers' do
+      project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      project1.save
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => project1.id, :id => nil})
+      volunteer1.save
+      volunteer2 = Volunteer.new({:name => 'Joe', :project_id => project1.id, :id => nil})
+      volunteer2.save
+      expect(Volunteer.all).to eq [volunteer1, volunteer2]
+    end
+  end
+
   describe '#save' do
     it 'adds a volunteer to the database' do
       project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
